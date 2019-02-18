f57f21411b5f8f67281b55f399cdefda3f50a60f
NuttyLogic <colinpfarrell@gmail.com>
2019-02-18 10:22:55 -0800

    Update License to GPLv3

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-setup(name='BSBbolt',
+setup(name='BiSulfiteBolt',
       version='0.0.1',
       description='Bisulfite Sequencing Processing Platform',
       author='Colin Farrell',
       author_email='colinpfarrell@gmail.com',
       license='MIT',
+      packages=['BSBolt'],
       requires=['pysam', 'numpy', 'tqdm'])
