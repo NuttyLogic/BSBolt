e8455a09993b4c056d97f9338e85e15036f8fb98
Colin Farrell <colinpfarrell@gmail.com>
2019-12-13 09:30:39 -0800

    fixed ignore orphan call not being properly passed to process contig instance

--- tests/test_methylation_index.py
+++ tests/test_methylation_index.py
@@ -9,23 +9,22 @@ from BSBolt.Utils.UtilityFunctions import reverse_complement
 
 test_directory = os.path.dirname(os.path.realpath(__file__))
 bsb_directory = '/'.join(test_directory.split('/')[:-1]) + '/'
-bsbolt = f'{bsb_directory}BSBolt.py'
 
 # generate methylation indices for rrbs, wgbs, and wgbs masked
 
-bsb_wgbs_index_commands = ['python3', bsbolt, 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
+bsb_wgbs_index_commands = ['python3', '-m', 'BSBolt', 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
                            '-DB', f'{bsb_directory}Tests/TestData/BSB_Test_DB_wgbs']
 
 subprocess.run(bsb_wgbs_index_commands)
 
-bsb_wgbs_masked_index_commands = ['python3', bsbolt, 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
+bsb_wgbs_masked_index_commands = ['python3', '-m', 'BSBolt', 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
                                   '-DB', f'{bsb_directory}Tests/TestData/BSB_Test_DB_wgbs_masked', '-MR',
                                   f'{bsb_directory}Tests/TestData/test_wgbs_masking.bed']
 
 subprocess.run(bsb_wgbs_masked_index_commands)
 
 
-bsb_rrbs_index_commands = ['python3', bsbolt, 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
+bsb_rrbs_index_commands = ['python3', '-m', 'BSBolt', 'Index', '-G', f'{bsb_directory}Tests/TestData/BSB_test.fa',
                            '-DB', f'{bsb_directory}Tests/TestData/BSB_Test_DB_rrbs', '-rrbs', '-MR',
                            f'{bsb_directory}Tests/TestData/test_wgbs_masking.bed', '-rrbs-cut-format', 'CAG-G']
 
